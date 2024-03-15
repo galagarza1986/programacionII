e00c87e55588322646dd4f3a7ae8838b6da05c6d
galagarza1986 <aaron.galagarzacarrillo@ucr.ac.cr>
2024-03-15 08:35:56 -0600

    primera carga

---
+++ src/main/java/ucr/ac/cr/programacionii/ProgramacionII.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ucr.ac.cr.programacionii;
+
+/**
+ *
+ * @author galagarza
+ */
+public class ProgramacionII {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
